0a1b048d8db9318e4178a5292dfa517141af41b5
Ayush Anand <ayushanand3107@gmail.com>
2025-08-20 14:08:36 +0530

    Initial commit

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import pandas as pd
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-'''if __name__ == '__main__':
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)'''
+    app.run(host='0.0.0.0', port=port)
 
 
 
